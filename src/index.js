717f6caab380c7c6e602eee5039525b2878bc667
sunwithcloud <exentkill@gmail.com>
2022-02-08 23:10:44 +0300

    feat: compleate task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = [...str];
+    
+    for(let i = 0; i < arr.length; i++) {
+        for(let j=0; j<bracketsConfig.length; j++){
+            if(arr[i] === bracketsConfig[j][0] && arr[i + 1] === bracketsConfig[j][1]){
+                arr.splice(i, 2);
+                i =- 1;
+            }
+        }
+    }
+    if (arr.length==0){
+        return true;
+    }else{
+        return false;
+    }
 }
